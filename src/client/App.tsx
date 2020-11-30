01d76673879a1a6cca8f33108c91218dc05ecb75
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2020-11-30 11:42:08 -0600

    updated App.tsx to include AdminResumeView, added same as a component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -8,9 +8,13 @@ import Footer from './components/Footer';
 import Login from './components/Login';
 import Home from './components/Home';
 import Profile from './components/Profile';
+
 import Resume from './components/Resume';
 import InterviewPrep from './components/InterviewPrep';
 import Flashcards from './components/Flashcards';
+
+import AdminResumeView from './components/AdminResumeView';
+
 
 // import Admin from './components/Admin';
 // import Resources from './components/Resources';
@@ -28,12 +32,16 @@ const App: React.FC<IAppProps> = props => {
 			<main className="container-fluid">
 				
 				<Switch>
+					
 					<Route exact path="/" component={Login} />
 					<Route exact path="/home" component={Home} />
 					<Route exact path="/profile/:id" component={Profile} />
 					<Route exact path="/resume/:id" component={Resume} />
 					<Route exact path="/interviewprep/:id" component={InterviewPrep} />
 					<Route exact path="/flashcards" component={Flashcards} />
+					
+					<Route exact path="/adminresumeview" component={AdminResumeView} />
+
 				</Switch>
 
 			</main>
