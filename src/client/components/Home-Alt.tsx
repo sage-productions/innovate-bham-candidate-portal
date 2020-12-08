c525ac52c3b16009fb4cc98727feb835384eae7b
jAndrew78 <58049852+jAndrew78@users.noreply.github.com>
2020-12-08 10:45:22 -0600

    added auth check - commented out

--- src/client/components/Home-Alt.tsx
+++ src/client/components/Home-Alt.tsx
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { RouteComponentProps, Link } from "react-router-dom";
 import "../scss/home-alt.scss";
+import { User } from "../utils/api";
 
 const Home: React.FC<HomeProps> = (props) => {
+
+  // useEffect(() => {
+  //   if(!User || User.userid === null || User.role !== 'admin') {
+  //     props.history.replace('/')
+  //   }
+  // }, [])
+
   return (
     <main className="container-fluid text-midnight">
       <div className="row title-row">
@@ -155,6 +163,6 @@ const Home: React.FC<HomeProps> = (props) => {
   );
 };
 
-interface HomeProps {}
+interface HomeProps extends RouteComponentProps {}
 
 export default Home;
